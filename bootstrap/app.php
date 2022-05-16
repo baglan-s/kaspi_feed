7fa5a72781c75fc01d9fec38b1f6315f0ad8f5d6
baglan <bsariev@hiplabs.dev>
2022-05-16 17:01:19 +0600

    added lumen cors 2

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -60,6 +60,7 @@ $app->singleton(
 */
 
 $app->configure('app');
+$app->configure('cors');
 
 /*
 |--------------------------------------------------------------------------
@@ -74,7 +75,7 @@ $app->configure('app');
 
 $app->middleware([
     App\Http\Middleware\ExampleMiddleware::class,
-    'Nord\Lumen\Cors\CorsMiddleware',
+    Fruitcake\Cors\HandleCors::class,
 ]);
 
 $app->routeMiddleware([
@@ -96,7 +97,7 @@ $app->register('Sentry\Laravel\ServiceProvider');
 // $app->register(App\Providers\AppServiceProvider::class);
 $app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
-$app->register('Nord\Lumen\Cors\CorsServiceProvider');
+$app->register(Fruitcake\Cors\CorsServiceProvider::class);
 
 /*
 |--------------------------------------------------------------------------
